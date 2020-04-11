cba0fd4887d76df8c84b2b34c22b523ac223aa05
Marco Giordano <marco.giordano.work@gmail.com>
2020-04-11 15:28:00 +0100

    started working on metacompiler to generate classes for the AST

--- build-win.bat
+++ build-win.bat
@@ -1,12 +1,27 @@
 @echo off
 
+REM setting up the folders and data 
 mkdir build\mono
+mkdir core\src\autogen
 xcopy /y /E tests\testData build\testData\ 
 
+REM build the meta compiler
+clang++ -g -O0 -Wall -std=c++17 -Wno-format-security -Wno-deprecated-declarations -o build/mono/metacompiler.exe -I core/includes tools/metacompiler/main.cpp
+echo -- metacompiler built
+
+REM run the metacompiler
+pushd build\mono
+.\metacompiler.exe
+popd
+echo -- metacompiler run
+
+REM build the core library 
 clang++ -g -O0 -c -Wall -std=c++17 -Wno-deprecated-declarations -o build/mono/core.o -I core/includes core/src/main.cpp
 llvm-ar rc build/mono/core.lib build/mono/core.o
+echo -- core built
 
 clang++ -g -O0 -Wall -std=c++17 -Wno-deprecated-declarations -o build/mono/Tests.exe -I core/includes tests/src/main.cpp build/mono/core.lib
+echo -- test built
 pushd build\mono
 .\Tests.exe
 popd
