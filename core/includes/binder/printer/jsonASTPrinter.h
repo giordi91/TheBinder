3069e3a91c1c41ba63233c7f1e1808c11a795811
Marco Giordano <marco.giordano.work@gmail.com>
2020-04-16 18:04:06 +0100

    minor printer cleanup

--- core/includes/binder/printer/jsonASTPrinter.h
+++ core/includes/binder/printer/jsonASTPrinter.h
@@ -46,14 +46,13 @@ public:
     const char *title = m_pool.concatenate("{ \"type\" : \"", "\"", type);
     if (op != nullptr) {
       const char* opStr = m_pool.concatenate(",\"op\" :\"", "\"", op);
-      title = m_pool.concatenate(title, opStr);
+      title= m_pool.concatenate(title, opStr, nullptr, ff|sf);
     }
 
     const char *expr1str = (char *)expr1->accept(this);
-    // const char flags = binder::memory::FREE_FIRST_AFTER_OPERATION |
-    //                   binder::memory::FREE_SECOND_AFTER_OPERATION;
     const char *left = m_pool.concatenate(title, ",\"left\": ",nullptr,ff);
     const char *expr1done = m_pool.concatenate(left, expr1str,nullptr,ff|sf);
+    //TODO should clear this up and possibly remove the return in the if
     if (expr2 != nullptr) {
       // result from second expression
       char *expr2str = (char *)expr2->accept(this);
