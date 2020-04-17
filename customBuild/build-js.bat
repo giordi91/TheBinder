4777aa4eb6f586f5d962110125672b7dabadae12
Marco Giordano <marco.giordano.work@gmail.com>
2020-04-17 21:27:27 +0100

    first draft of the full html page

--- customBuild/build-js.bat
+++ customBuild/build-js.bat
@@ -7,7 +7,7 @@ REM copy the rest of the files like js and css out to the build folder
 xcopy /y /s /i ..\tools\emscripten\web\css ..\build\mono\js\css\
 xcopy /y /s /i ..\tools\emscripten\web\js  ..\build\mono\js\js\
 
-em++ -g0  -Wall -std=c++17  -I ../core/includes  ../core/src/main.cpp ../tools/emscripten/main.cpp  -o ../build\mono\js\bind5.html --shell-file ../tools/emscripten/web/wasm.html  -s MINIFY_HTML=0 -s WASM=1   -s EXPORTED_FUNCTIONS=["_bindExecute"] -s EXTRA_EXPORTED_RUNTIME_METHODS=["cwrap","ccall"]  -s NO_EXIT_RUNTIME=1 
+em++ -g0  -Wall -std=c++17  -I ../core/includes  ../core/src/main.cpp ../tools/emscripten/main.cpp  -o ../build\mono\js\bind.html --shell-file ../tools/emscripten/web/wasm.html  -s MINIFY_HTML=0 -s WASM=1   -s EXPORTED_FUNCTIONS=["_bindExecute"] -s EXTRA_EXPORTED_RUNTIME_METHODS=["cwrap","ccall"]  -s NO_EXIT_RUNTIME=1 
 
 
 
--- tools/emscripten/web/js/glue.js
+++ tools/emscripten/web/js/glue.js
@@ -16,9 +16,24 @@ function initPage() {
           var content = this.nextElementSibling;
           if (content.style.display === "block") {
             content.style.display = "none";
+            this.innerHTML="&#x25B6; AST";
           } else {
             content.style.display = "block";
+            this.innerHTML="&#x25BC; AST";
           }
         });
   }
+}
+function openActionTab(evt, actionName) {
+      var i, tabcontent, tablinks;
+      tabcontent = document.getElementsByClassName("tabcontent");
+      for (i = 0; i < tabcontent.length; i++) {
+              tabcontent[i].style.display = "none";
+            }
+      tablinks = document.getElementsByClassName("tablinks");
+      for (i = 0; i < tablinks.length; i++) {
+              tablinks[i].className = tablinks[i].className.replace(" active", "");
+            }
+      document.getElementById(actionName).style.display = "block";
+      evt.currentTarget.className += " active";
 }
