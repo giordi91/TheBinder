fc1606e546fb7c768aad3e092817bdb22df5cf80
Marco Giordano <marco.giordano.work@gmail.com>
2020-05-08 22:10:29 +0100

    added lots of tests for the parser assigment and started to test branches a bit

--- tests/src/parserTests.cpp
+++ tests/src/parserTests.cpp
@@ -63,6 +63,28 @@ const binder::autogen::Binary *compareBinary(const binder::autogen::Expr *expr,
   return bin;
 }
 
+template <typename L, typename R>
+const binder::autogen::Logical *compareLogical(const binder::autogen::Expr *expr,
+                                             binder::TOKEN_TYPE expectedOp,
+                                             const L **lhs, const R **rhs) {
+  auto *logical = dynamic_cast<const binder::autogen::Logical*>(expr);
+  REQUIRE(logical!= nullptr);
+  REQUIRE(logical->op == expectedOp);
+
+  *lhs = dynamic_cast<const L *>(logical->left);
+  *rhs = dynamic_cast<const R *>(logical->right);
+
+  REQUIRE(lhs != nullptr);
+  REQUIRE(rhs != nullptr);
+  return logical;
+}
+
+//template <typename L, typename R>
+//const binder::autogen::If *compareIf(const binder::autogen::Stmt *stmt,
+//                                             const L **lhs, const R **rhs) {
+//}
+
+
 template <typename L>
 const binder::autogen::Unary *compareUnary(const binder::autogen::Expr *expr,
                                            binder::TOKEN_TYPE expectedType,
@@ -76,13 +98,24 @@ const binder::autogen::Unary *compareUnary(const binder::autogen::Expr *expr,
   return unary;
 }
 
+const binder::autogen::Variable* compareVariable(const binder::autogen::Expr *expr,
+                                           const char* expectedVar
+                                           ) 
+{
+  auto *var = dynamic_cast<const binder::autogen::Variable*>(expr);
+  REQUIRE(var != nullptr);
+
+  REQUIRE(strcmp(var->name,expectedVar)==0);
+  return var;
+}
+
 TEST_CASE_METHOD(SetupParserTestFixture, "basic number parse", "[parser]") {
 
   const binder::memory::ResizableVector<binder::autogen::Stmt *> &stmts =
       parse("12345.5;");
   REQUIRE(stmts.size() == 1);
 
-  auto *stmt= dynamic_cast<binder::autogen::Expression *>(stmts[0]);
+  auto *stmt = dynamic_cast<binder::autogen::Expression *>(stmts[0]);
   compareLiteral(stmt->expression, binder::TOKEN_TYPE::NUMBER, "12345.5");
 }
 
@@ -90,10 +123,10 @@ TEST_CASE_METHOD(SetupParserTestFixture, "basic multiply", "[parser]") {
 
   // no space in the rhs
   const binder::memory::ResizableVector<binder::autogen::Stmt *> &stmts =
-   parse("77 *323.2;");
+      parse("77 *323.2;");
   REQUIRE(stmts.size() == 1);
-  auto *root= (dynamic_cast<binder::autogen::Expression
-*>(stmts[0]))->expression;
+  auto *root =
+      (dynamic_cast<binder::autogen::Expression *>(stmts[0]))->expression;
 
   const binder::autogen::Literal *l = nullptr;
   const binder::autogen::Literal *r = nullptr;
@@ -178,11 +211,10 @@ TEST_CASE_METHOD(SetupParserTestFixture, "MAD 2", "[parser]") {
 
   compareBinary(grpLhs->expr, binder::TOKEN_TYPE::STAR, &unary, &literal);
 
-  const binder::autogen::Literal *literalLhs= nullptr;
-  compareUnary(unary,binder::TOKEN_TYPE::MINUS, &literalLhs);
+  const binder::autogen::Literal *literalLhs = nullptr;
+  compareUnary(unary, binder::TOKEN_TYPE::MINUS, &literalLhs);
   compareLiteral(literalLhs, binder::TOKEN_TYPE::NUMBER, "1");
   compareLiteral(literal, binder::TOKEN_TYPE::NUMBER, "3.14");
-
 
   // now we can go down the initial binary rhs, the (--13) part
   // grpRhs->expr is already the first unary, so comparing a unary and
@@ -196,14 +228,12 @@ TEST_CASE_METHOD(SetupParserTestFixture, "MAD 2", "[parser]") {
 
 TEST_CASE_METHOD(SetupParserTestFixture, "expression error 1", "[parser]") {
 
-
   context.setErrorReportingEnabled(false);
   const binder::memory::ResizableVector<binder::autogen::Stmt *> &stmts =
       parse("1 ** 1;");
   REQUIRE(stmts.size() == 0);
-  REQUIRE(context.hadError()==true);
+  REQUIRE(context.hadError() == true);
   context.setErrorReportingEnabled(true);
-
 }
 
 TEST_CASE_METHOD(SetupParserTestFixture, "expression error 2", "[parser]") {
@@ -212,38 +242,136 @@ TEST_CASE_METHOD(SetupParserTestFixture, "expression error 2", "[parser]") {
   const binder::memory::ResizableVector<binder::autogen::Stmt *> &stmts =
       parse("(1 * 1;");
   REQUIRE(stmts.size() == 0);
-  REQUIRE(context.hadError()==true);
+  REQUIRE(context.hadError() == true);
   context.setErrorReportingEnabled(true);
-
 }
 
 TEST_CASE_METHOD(SetupParserTestFixture, "expression error 3", "[parser]") {
-
 
   context.setErrorReportingEnabled(false);
   const binder::memory::ResizableVector<binder::autogen::Stmt *> &stmts =
       parse("-1 * \"t\"");
   REQUIRE(stmts.size() == 0);
-  REQUIRE(context.hadError()==true);
+  REQUIRE(context.hadError() == true);
   context.setErrorReportingEnabled(true);
-
 }
 
-
-TEST_CASE_METHOD(SetupParserTestFixture, "assigment", "[parser]") 
-{
+TEST_CASE_METHOD(SetupParserTestFixture, "assigment error", "[parser]") {
   const binder::memory::ResizableVector<binder::autogen::Stmt *> &stmts =
       parse("var a = 1; a = 2;");
   REQUIRE(stmts.size() == 2);
-  REQUIRE(context.hadError()==false);
+  REQUIRE(context.hadError() == false);
+}
+
+TEST_CASE_METHOD(SetupParserTestFixture, "assigment number", "[parser]") {
+  const binder::memory::ResizableVector<binder::autogen::Stmt *> &stmts =
+      parse("var a = 1;");
+  REQUIRE(stmts.size() == 1);
+  REQUIRE(context.hadError() == false);
+
+  REQUIRE(stmts.size() == 1);
+  auto *var = dynamic_cast<binder::autogen::Var *>(stmts[0]);
+  REQUIRE(var != nullptr);
+  auto *root = var->initializer;
+  REQUIRE(root != nullptr);
+  compareLiteral(root, binder::TOKEN_TYPE::NUMBER, "1");
+}
+
+TEST_CASE_METHOD(SetupParserTestFixture, "assigment expr ", "[parser]") {
+  const binder::memory::ResizableVector<binder::autogen::Stmt *> &stmts =
+      parse("var a = 1 + 3;");
+  REQUIRE(stmts.size() == 1);
+  REQUIRE(context.hadError() == false);
+
+  REQUIRE(stmts.size() == 1);
+  auto *var = dynamic_cast<binder::autogen::Var *>(stmts[0]);
+  REQUIRE(var != nullptr);
+  auto *root = var->initializer;
+  REQUIRE(root != nullptr);
+
+  const binder::autogen::Literal *lhs= nullptr;
+  const binder::autogen::Literal *rhs= nullptr;
+  compareBinary(root, binder::TOKEN_TYPE::PLUS, &lhs, &rhs);
+  compareLiteral(lhs, binder::TOKEN_TYPE::NUMBER, "1");
+  compareLiteral(rhs, binder::TOKEN_TYPE::NUMBER, "3");
+
+}
+TEST_CASE_METHOD(SetupParserTestFixture, "assigment variable", "[parser]") {
+  const binder::memory::ResizableVector<binder::autogen::Stmt *> &stmts =
+      parse("var a = b;");
+  REQUIRE(stmts.size() == 1);
+  REQUIRE(context.hadError() == false);
+
+  REQUIRE(stmts.size() == 1);
+  auto *var = dynamic_cast<binder::autogen::Var *>(stmts[0]);
+  REQUIRE(var != nullptr);
+  auto *root = dynamic_cast<binder::autogen::Variable *>(var->initializer);
+  REQUIRE(root != nullptr);
+
+  compareVariable(root, "b");
 
 }
 
-TEST_CASE_METHOD(SetupParserTestFixture, "or operator", "[parser]") 
-{
+TEST_CASE_METHOD(SetupParserTestFixture, "or operator", "[parser]") {
   const binder::memory::ResizableVector<binder::autogen::Stmt *> &stmts =
       parse("var a = 1 or 2;");
   REQUIRE(stmts.size() == 1);
-  REQUIRE(context.hadError()==false);
+  REQUIRE(context.hadError() == false);
+
+  REQUIRE(stmts.size() == 1);
+  auto *var = dynamic_cast<binder::autogen::Var *>(stmts[0]);
+  REQUIRE(var != nullptr);
+  auto *root = var->initializer;
+  REQUIRE(root != nullptr);
+
+  const binder::autogen::Literal *l = nullptr;
+  const binder::autogen::Literal *r = nullptr;
+
+  compareLogical(root, binder::TOKEN_TYPE::OR, &l, &r);
+  compareLiteral(l, binder::TOKEN_TYPE::NUMBER, "1");
+  compareLiteral(r, binder::TOKEN_TYPE::NUMBER, "2");
+}
+
+TEST_CASE_METHOD(SetupParserTestFixture, "and operator", "[parser]") {
+  const binder::memory::ResizableVector<binder::autogen::Stmt *> &stmts =
+      parse("var x = 11 and 22;");
+  REQUIRE(stmts.size() == 1);
+  REQUIRE(context.hadError() == false);
+
+  REQUIRE(stmts.size() == 1);
+  auto *var = dynamic_cast<binder::autogen::Var *>(stmts[0]);
+  REQUIRE(var != nullptr);
+  auto *root = var->initializer;
+  REQUIRE(root != nullptr);
+
+  const binder::autogen::Literal *l = nullptr;
+  const binder::autogen::Literal *r = nullptr;
+
+  compareLogical(root, binder::TOKEN_TYPE::AND, &l, &r);
+  compareLiteral(l, binder::TOKEN_TYPE::NUMBER, "11");
+  compareLiteral(r, binder::TOKEN_TYPE::NUMBER, "22");
+}
+
+TEST_CASE_METHOD(SetupParserTestFixture, "if only branch", "[parser]") {
+  const binder::memory::ResizableVector<binder::autogen::Stmt *> &stmts =
+      parse("if ( x or y){ var x = 1;}");
+  REQUIRE(stmts.size() == 1);
+  REQUIRE(context.hadError() == false);
+
+  REQUIRE(stmts.size() == 1);
+  auto *ifstmt = dynamic_cast<binder::autogen::If*>(stmts[0]);
+  REQUIRE(ifstmt != nullptr);
+
+  const auto*block = dynamic_cast<binder::autogen::Block*>(ifstmt->thenBranch);
+  REQUIRE(ifstmt->elseBranch == nullptr);
+  REQUIRE(block != nullptr);
+
+  REQUIRE(block->statements.size() == 1);
+
+  auto *var = dynamic_cast<binder::autogen::Var *>(block->statements[0]);
+  REQUIRE(var != nullptr);
+  auto *init= var->initializer;
+  REQUIRE(init != nullptr);
+  compareLiteral(init, binder::TOKEN_TYPE::NUMBER, "1");
 
 }
